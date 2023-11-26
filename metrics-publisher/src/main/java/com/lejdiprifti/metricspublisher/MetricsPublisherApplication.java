aa7fffd21d2f482912ff6bda6f554392aae6835f
Lejdi Prifti <lejdi.prifti@linksmt.it>
2023-11-26 19:44:16 +0100

    new article

---
+++ metrics-publisher/src/main/java/com/lejdiprifti/metricspublisher/MetricsPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.lejdiprifti.metricspublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsPublisherApplication.class, args);
+	}
+
+}
---
+++ metrics-publisher/src/test/java/com/lejdiprifti/metricspublisher/MetricsPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lejdiprifti.metricspublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
