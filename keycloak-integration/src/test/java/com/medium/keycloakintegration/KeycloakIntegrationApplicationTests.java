fc219a6c933e8902e3ca33654e9be58903214fca
Lejdi Prifti <lejdi.prifti@linksmt.it>
2024-04-01 01:01:11 +0200

    init keycloak integration

---
+++ keycloak-integration/src/test/java/com/medium/keycloakintegration/KeycloakIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.keycloakintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
