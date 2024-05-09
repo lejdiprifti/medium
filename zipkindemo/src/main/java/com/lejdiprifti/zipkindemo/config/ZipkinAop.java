cf3a1a28444d66150df16425f0ba4ea921b2421f
Lejdi Prifti <lejdi.prifti@linksmt.it>
2024-05-09 23:12:08 +0200

    added zipkinaop

---
+++ zipkindemo/src/main/java/com/lejdiprifti/zipkindemo/config/ZipkinAop.java
@@ -0,0 +1,42 @@
+package com.lejdiprifti.zipkindemo.config;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import brave.ScopedSpan;
+import brave.Tracer;
+import lombok.RequiredArgsConstructor;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class ZipkinAop {
+
+	private static final String METHOD_NAME = "method.name";
+
+	private final Tracer tracer;
+
+	@Around("execution(public * com.lejdiprifti..*(..))")
+	public Object traceAllMethodCalls(ProceedingJoinPoint pJoinPoint) throws Throwable {
+		final MethodSignature methodSignature = (MethodSignature) pJoinPoint.getSignature();
+		final ScopedSpan span = tracer.startScopedSpan(methodSignature.toShortString());
+		if (span != null) {
+			span.tag(METHOD_NAME, methodSignature.getName());
+		}
+		try {
+			return pJoinPoint.proceed();
+		} catch (Exception exception) {
+			if (span != null) {
+				span.error(exception);
+			}
+			throw exception;
+		} finally {
+			if (span != null) {
+				span.finish();
+			}
+		}
+	}
+}
